c916ae9bfe0f5820a7b790315e100cd619149ab1
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-29 13:18:39 +0530

    k8s workstation ec2 eks

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "k8s-workstation" {
   # convert StringList to list and get first element
   subnet_id = var.public_subnet_id
   user_data = file("workstation.sh")
-  iam_instance_profile = "k8s-iam-role-authentication"
+  iam_instance_profile = "k8s-iam-role-authentication" #administrator access role name.
 
   tags = {
         Name = "k8s-workstation-ec2"
@@ -31,24 +31,3 @@ records = [
       }
    ]
 }
-
-# resource "aws_instance" "expense" {
-#     for_each = var.instances
-#     ami           = data.aws_ami.rhel_info.id
-#     instance_type = each.value
-#     vpc_security_group_ids = [var.allow_all]
-#     user_data = file("${path.module}/install_${each.key}.sh")
-
-#     tags = {
-#         Name = each.key
-#     }
-# }
-# resource "aws_route53_record" "expense_r53" {
-#     for_each = aws_instance.expense
-#     zone_id = var.zone_id
-#     name    = each.key == "frontend"? var.domain_name : "${each.key}.${var.domain_name}" 
-#     type    = "A"
-#     ttl     = 1
-#     records = each.key == "frontend" ? [each.value.public_ip]:[each.value.private_ip]
-#     allow_overwrite = true
-# }
