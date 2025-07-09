db2e4397c167ab449e565cabd8333a24222597ed
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-09 17:17:15 +0530

    k8s workstation module ec2

--- main.tf
+++ main.tf
@@ -1,38 +1,78 @@
-module "k8s-workstation" {
+# EC2 Module: K8s Workstation
+module "k8s_workstation" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  name = "k8s-Workstation"
-
-  ami           = data.aws_ami.rhel_info.id
-  instance_type = var.k8s_instance.instance_type
-  vpc_security_group_ids = [var.allow-everything]
-  # convert StringList to list and get first element
-  subnet_id = var.public_subnet_id
-  user_data = file("workstation.sh")
-  iam_instance_profile = "k8s-iam-role-authentication" #administrator access role name.
-  root_block_device {
-    volume_size = 100  # Size of the root volume in GB
-    volume_type = "gp2"  # General Purpose SSD (you can change the volume type if needed)
-    delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
-
+  name    = "k8s-Workstation"
+  ami                    = data.aws_ami.rhel_info.id
+  instance_type          = var.k8s_instance.instance_type
+  vpc_security_group_ids = [var.allow_everything]
+  subnet_id              = var.public_subnet_id
+  user_data              = file("workstation.sh")
+  iam_instance_profile   = "k8s-iam-role-authentication"
+   root_block_device = {
+    volume_size           = 100
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
   tags = {
-        Name = "k8s-workstation-ec2"
-    }
+    Name = "k8s-workstation-ec2"
+  }
 }
-module "records" {
+
+# Route53 Module: DNS Record for K8s Workstation
+module "records"{
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
   zone_name = var.domain_name
-
-records = [
-      {
-        name = "k8s"
-        type = "A"
-        ttl  = 1
-        records = [
-          module.k8s-workstation.public_ip
-        ]
-        allow_overwrite = true
-      }
-   ]
+  records = [
+    {
+      name            = "k8s"
+      type            = "A"
+      ttl             = 1
+      records         = [module.k8s_workstation.public_ip]
+      allow_overwrite = true
+    }
+  ]
+  depends_on = [module.k8s_workstation]
 }
+
+
+
+# module "k8s-workstation" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   name = "k8s-Workstation"
+
+#   ami           = data.aws_ami.rhel_info.id
+#   instance_type = var.k8s_instance.instance_type
+#   vpc_security_group_ids = [var.allow-everything]
+#   # convert StringList to list and get first element
+#   subnet_id = var.public_subnet_id
+#   user_data = file("workstation.sh")
+#   iam_instance_profile = "k8s-iam-role-authentication" #administrator access role name.
+#   root_block_device = [
+#     {
+#       volume_size           = 100
+#       volume_type           = "gp2"
+#       delete_on_termination = true
+#     }
+#   ]
+#   tags = {
+#         Name = "k8s-workstation-ec2"
+#     }
+# }
+# module "records" {
+#   source  = "terraform-aws-modules/route53/aws//modules/records"
+#   version = "~> 2.0"
+#   zone_name = var.domain_name
+
+# records = [
+#       {
+#         name = "k8s"
+#         type = "A"
+#         ttl  = 1
+#         records = [
+#           module.k8s-workstation.public_ip
+#         ]
+#         allow_overwrite = true
+#       }
+#    ]
+# }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "allow-everything" {
+variable "allow_everything" {
     type = string
     default = "sg-09c7c70bd56f0d58b"
 }
